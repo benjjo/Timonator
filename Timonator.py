5b3dde44096fc87287c7eb1c841d603f3f4cc68c
benjjo <benjjo@gmail.com>
2021-09-10 16:12:04 +0100

    FDS plots ok. BUG - Empty folder created for FDS.

--- Timonator.py
+++ Timonator.py
@@ -72,8 +72,6 @@ def convert_bitsets_to_int():
 
 
 def add_bitset_sub_columns(df):
-    os.mkdir('./Plots')
-
     if 'ASDO_StsW(Bitset16)' in df.columns:
         df['Formation_OK'] = df['ASDO_StsW(Bitset16)'].apply(lambda x: Formation_OK(x))
     if 'PLC_EVR_BS1(Bitset8)' in df.columns:
@@ -83,14 +81,16 @@ def add_bitset_sub_columns(df):
     if 'PLC_PIS_CMD1(Bitset8)' in df.columns:
         df['DoorSideA'] = df['PLC_PIS_CMD1(Bitset8)'].apply(lambda x: DoorSideA(x))
         df['DoorSideB'] = df['PLC_PIS_CMD1(Bitset8)'].apply(lambda x: DoorSideB(x))
+    return df
 
 
-def save_individual_plots_to_png(list_of_cols, df, remove_time_columns=True):
+def save_individual_plots_to_png(list_of_cols, df, remove_time_columns=True, folder_name='Timon_Plots'):
     """
     Takes a list and a data frame and makes a bunch of plots for each of the individual variables.
     Removes time/date columns from the list by default.
     Iterates over the columns to
     """
+    os.mkdir(folder_name)
     if remove_time_columns:
         for col in list_of_cols:
             if col in ['PLC_TIME(Timedate48)', 'Time Date', 'TIME', 'PLC_TIME_CV(Enum2)']:
@@ -99,18 +99,18 @@ def save_individual_plots_to_png(list_of_cols, df, remove_time_columns=True):
     for col in tqdm(list_of_cols):
         if col == 'PLC_MASTER_COACH(Unsigned16)':
             df[col].plot(figsize=(16, 4), legend=True, ylim=(15000, 15011))
-            plt.savefig('Plots/' + col, dpi=300, facecolor='w', edgecolor='w', orientation='landscape', format=None,
+            plt.savefig(folder_name, dpi=300, facecolor='w', edgecolor='w', orientation='landscape', format=None,
                         transparent=False, pad_inches=0.1)
             plt.close()
         elif col in ['Formation_OK', 'PLC_InauFinished', 'Degraded_Mode', 'DoorSideA', 'DoorSideB',
                      'HMI_CONFIRM_CMD(Boolean1)', 'HMI_RECALCULATE_CMD(Boolean1)'] or 'Boolean' in col:
             df[col].plot(figsize=(16, 4), legend=True, ylim=(0, 1))
-            plt.savefig('Plots/' + col, dpi=300, facecolor='w', edgecolor='w', orientation='landscape',
+            plt.savefig(folder_name, dpi=300, facecolor='w', edgecolor='w', orientation='landscape',
                         format=None, transparent=False, pad_inches=0.1)
             plt.close()
         else:
             df[col].plot(figsize=(16, 4), legend=True)
-            plt.savefig('Plots/' + col, dpi=300, facecolor='w', edgecolor='w', orientation='landscape',
+            plt.savefig(folder_name, dpi=300, facecolor='w', edgecolor='w', orientation='landscape',
                         format=None, transparent=False, pad_inches=0.1)
             plt.close()
 
@@ -123,9 +123,10 @@ def lifeword_plot(df: pd.DataFrame, file_name: str, lifeword: str):
             if search_word in col:
                 lw_list.append(col)
     lw_df = df[lw_list]
+    title_name = file_name.split('/')[-1]
     save_individual_plots_to_png(list_of_cols=lw_list, df=lw_df)
     lw_df.plot(subplots=True, figsize=(16, 8), legend=True, xlabel='Time Date',
-               title=f'{lifeword} Lifeword :: {file_name}')
+               title=f'{lifeword} Lifeword :: {title_name}')
     plt.show()
 
 
@@ -209,10 +210,12 @@ def print_group():
 
 
 def setup_dataframe():
+    global global_df
     set_timon_log_filename()
     set_global_df(read_and_clean_timon_log())
     create_datetime_column()
-    add_bitset_sub_columns()
+    convert_bitsets_to_int()
+    set_global_df(add_bitset_sub_columns(global_df))
 
 
 def main():
@@ -243,17 +246,17 @@ def main():
   | |  | || | | | | || (_) || | | || (_| || |_ | (_) || |
   |_|  |_||_| |_| |_| \___/ |_| |_| \__,_| \__| \___/ |_| V1.0 alpha
                            ______
-                         <((((((\\\
-                         /      . }\
+                         <((((((\\\\\\
+                         /      . }\\
                          ;--..--._|}
       (\                 '--/\--'  )
-       \\                | '-'  :'|
-        \\               . -==- .-|
-         \\               \.__.'   \--._
-         [\\          __.--|       //  _/'--.
-         \ \\       .'-._ ('-----'/ __/      \
-          \ \\     /   __>|      | '--.       |
-           \ \\   |   \   |     /    /       /
+       \\\\                | '-'  :'|
+        \\\\               . -==- .-|
+         \\\\               \.__.'   \--._
+         [\\\\          __.--|       //  _/'--.
+         \ \\\\       .'-._ ('-----'/ __/      \\
+          \ \\\\     /   __>|      | '--.       |
+           \ \\\\   |   \   |     /    /       /
             \ '\ /     \  |     |  _/       /
              \  \       \ |     | /        /
               \  \       \       /
